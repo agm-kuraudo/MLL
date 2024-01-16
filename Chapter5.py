aedf41ebe33c10ac74db07d01d94620837ed1d03
agm-kuraudo <122623005+agm-kuraudo@users.noreply.github.com>
2024-01-16 13:36:59 +0000

    PD-146 - text to sequences added

--- Chapter5.py
+++ Chapter5.py
@@ -12,3 +12,7 @@ tokenizer.fit_on_texts(sentences)
 word_index = tokenizer.word_index
 print(word_index)
 # {'today': 1, 'is': 2, 'a': 3, 'day': 4, 'sunny': 5, 'rainy': 6}
+
+sequences = tokenizer.texts_to_sequences(sentences)
+print(sequences)
+# [[1, 2, 3, 5, 4], [1, 2, 3, 6, 4]]
