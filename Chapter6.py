947a5c6406849f9d95e8aee29b2a7650773ea063
agm-kuraudo <122623005+agm-kuraudo@users.noreply.github.com>
2024-06-28 10:45:16 +0100

    PD-147 - Sarcasm Embeddings

---
+++ Chapter6.py
@@ -0,0 +1,101 @@
+import urllib.request
+
+import tensorflow as tf
+
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+from bs4 import BeautifulSoup
+import string
+
+stopwords = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at",
+             "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do",
+             "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have", "having",
+             "he", "hed", "hes", "her", "here", "heres", "hers", "herself", "him", "himself", "his", "how",
+             "hows", "i", "id", "ill", "im", "ive", "if", "in", "into", "is", "it", "its", "itself",
+             "lets", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought",
+             "our", "ours", "ourselves", "out", "over", "own", "same", "she", "shed", "shell", "shes", "should",
+             "so", "some", "such", "than", "that", "thats", "the", "their", "theirs", "them", "themselves", "then",
+             "there", "theres", "these", "they", "theyd", "theyll", "theyre", "theyve", "this", "those", "through",
+             "to", "too", "under", "until", "up", "very", "was", "we", "wed", "well", "were", "weve", "were",
+             "what", "whats", "when", "whens", "where", "wheres", "which", "while", "who", "whos", "whom", "why",
+             "whys", "with", "would", "you", "youd", "youll", "youre", "youve", "your", "yours", "yourself",
+             "yourselves"]
+
+table = str.maketrans('', '', string.punctuation)
+
+# urllib.request.urlretrieve("https://storage.googleapis.com/learning-datasets/sarcasm.json", "sarcasm.json")
+
+import json
+
+
+sentences = []
+labels = []
+urls = []
+
+with open("/app/tmp/sarcasm.json", "r") as f:
+    datastore = json.load(f)
+
+    for item in datastore:
+        sentence = item["headline"].lower()
+        label = item['is_sarcastic']
+        link = item['article_link']
+        sentence = sentence.replace("-", " - ")
+        sentence = sentence.replace("/", " / ")
+        soup = BeautifulSoup(sentence)
+        sentence = soup.get_text()
+        words = sentence.split()
+        filtered_sentence = ""
+        for word in words:
+            word = word.translate(table)
+            if word not in stopwords:
+                filtered_sentence = filtered_sentence + word + " "
+        sentences.append(filtered_sentence)
+        labels.append(item['is_sarcastic'])
+        urls.append(item['article_link'])
+
+training_size = 23000
+
+training_sentences = sentences[0:training_size]
+testing_sentences = sentences[training_size:]
+training_labels = labels[0:training_size]
+testing_labels = labels[training_size:]
+
+vocab_size = 20000
+max_length = 10
+trunc_type='post'
+padding_type='post'
+oov_token = '<OOV>'
+
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_token)
+tokenizer.fit_on_texts(training_sentences)
+word_index = tokenizer.word_index
+
+training_sequences = tokenizer.texts_to_sequences(training_sentences)
+training_padded = pad_sequences(training_sequences, padding='post')
+
+testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
+testing_padded = pad_sequences(testing_sequences, padding='post')
+
+# print(word_index)
+
+import numpy as np
+training_padded = np.array(training_padded)
+training_labels = np.array(training_labels)
+testing_padded = np.array(testing_padded)
+testing_labels = np.array(testing_labels)
+
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_token)
+# To initialise an embedding layers you need the vocab size and a specified number of embedding dimensions.
+# The dimensions will be learned over time as the training data is matched to labels
+# tf.keras.layers.Embedding(vocab_size, embedding_dim)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(10000, 16),
+    tf.keras.layers.GlobalAveragePooling1D(),
+    tf.keras.layers.Dense(24, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.summary()
