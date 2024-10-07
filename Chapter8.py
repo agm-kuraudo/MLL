1aa23ebffc3aa17c7dfef4b5c2104695ca8a6008
agm-kuraudo <122623005+agm-kuraudo@users.noreply.github.com>
2024-10-07 14:58:21 +0100

    PD-149 - Compounding Predictions to Generate Text

--- Chapter8.py
+++ Chapter8.py
@@ -63,3 +63,21 @@ for word, index in tokenizer.word_index.items():
 	if index == predicted:
 		print(word)
 		break
+
+seed_text = "sweet jeremy saw dublin"
+next_words=10
+token_list = tokenizer.texts_to_sequences([seed_text])[0]
+token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
+
+for _ in range(next_words):
+	token_list = tokenizer.texts_to_sequences([seed_text])[0]
+	token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
+	predicted = np.argmax(model.predict(token_list), axis=-1)
+	output_word = ""
+	for word, index in tokenizer.word_index.items():
+		if index == predicted:
+			output_word = word
+			break
+	seed_text += " " + output_word
+
+print(seed_text)
