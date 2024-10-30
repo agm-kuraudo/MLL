9652ebab85bc4732fa7177e34ea8c2ba9d9c5c17
Aiden Marriott <aiden@amainit.co.uk>
2024-10-30 09:41:32 +0000

    PD-150 - Finished chapter 9

--- chapter9.py
+++ chapter9.py
@@ -1,9 +1,10 @@
+#import keras
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib
+# import matplotlib
 import tensorflow as tf
 from tensorflow import keras
-import time
+# import time
 
 def plot_series(time, series, format="-", start=0, end=None):
     plt.plot(time[start:end], series[start:end], format)
@@ -49,5 +50,94 @@ series += noise(time_1, noise_level, seed=42)
 #matplotlib.use('TkAgg')
 plt.figure(figsize=(10, 6))
 plot_series(time_1, series)
-plt.savefig("/app/tmp/chart2.png")
+#plt.savefig("/app/tmp/chart2.png")
+plt.show()
 #time.sleep(120)
+
+
+
+
+split_time = 1000
+time_train = time_1[:split_time]
+x_train = series[:split_time]
+time_valid = time_1[split_time:]
+x_valid = series[split_time:]
+plt.figure(figsize=(10, 6))
+plot_series(time_train, x_train)
+plt.show()
+
+plt.figure(figsize=(10, 6))
+plot_series(time_valid, x_valid)
+plt.show()
+
+
+
+
+naive_forecast = series[split_time - 1:-1]
+
+
+plt.figure(figsize=(10, 6))
+plot_series(time_valid, x_valid)
+plot_series(time_valid, naive_forecast)
+
+
+plt.figure(figsize=(10, 6))
+plot_series(time_valid, x_valid, start=0, end=150)
+plot_series(time_valid, naive_forecast, start=1, end=151)
+
+
+m = keras.metrics.MeanSquaredError()
+m.update_state(x_valid, naive_forecast)
+print(m.result())
+
+m = keras.metrics.MeanAbsoluteError()
+m.update_state(x_valid, naive_forecast)
+print(m.result())
+
+
+def moving_average_forecast(series, window_size):
+  """Forecasts the mean of the last few values.
+     If window_size=1, then this is equivalent to naive forecast"""
+  forecast = []
+  for time in range(len(series) - window_size):
+    forecast.append(series[time:time + window_size].mean())
+  return np.array(forecast)
+
+
+moving_avg = moving_average_forecast(series, 30)[split_time - 30:]
+
+plt.figure(figsize=(10, 6))
+plot_series(time_valid, x_valid)
+plot_series(time_valid, moving_avg)
+plt.show()
+
+m = keras.metrics.MeanSquaredError()
+m.update_state(x_valid, moving_avg)
+print(m.result())
+
+m = keras.metrics.MeanAbsoluteError()
+m.update_state(x_valid, moving_avg)
+print(m.result())
+
+
+
+diff_series = (series[365:] - series[:-365])
+diff_time = time_1[365:]
+
+
+diff_moving_avg = moving_average_forecast(diff_series, 50)[split_time - 365 - 50:]
+
+diff_moving_avg_plus_smooth_past = moving_average_forecast(series[split_time - 370:-360], 10) + diff_moving_avg
+
+plt.figure(figsize=(10, 6))
+plot_series(time_valid, x_valid)
+plot_series(time_valid, diff_moving_avg_plus_smooth_past)
+plt.show()
+
+m = keras.metrics.MeanSquaredError()
+m.update_state(x_valid, diff_moving_avg_plus_smooth_past)
+print(m.result())
+
+m = keras.metrics.MeanAbsoluteError()
+m.update_state(x_valid, diff_moving_avg_plus_smooth_past)
+print(m.result())
